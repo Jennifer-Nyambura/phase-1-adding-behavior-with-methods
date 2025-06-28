e8a09fb3f74f0349e94e4bdbcc80413dfa3affab
Jennifer <jenninyambu@gmail.com>
2025-06-28 13:41:16 +0300

    all changes

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+class Cat {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+
+  speak() {
+    if (this.breed === "parrot") {
+      return `It's me! ${this.name}, the parrot!`;
+    } else {
+      return `${this.name} says squawk!`;
+    }
+  }
+}
+
+// Exporting classes for tests
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = { Cat, Dog, Bird };
+}
